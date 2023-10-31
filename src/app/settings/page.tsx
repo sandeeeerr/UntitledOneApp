ef69084da9ca198e5d797af88e9c08b3773803eb
sandeeeerr <sandervries@me.com>
2023-10-31 12:39:01 +0100

    1.0.3

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -2,7 +2,9 @@ import { redirect } from 'next/navigation'
 
 
 import { authOptions, getAuthSession } from '@/lib/auth'
-
+import Link from 'next/link'
+import {Button, buttonVariants} from "@element/Button";
+import { cn } from "@/lib/utils";
 
 export const metadata = {
   title: 'Settings',
@@ -19,14 +21,17 @@ export default async function SettingsPage() {
   return (
     <>
       <h1>settings</h1>
-
+      <Link href='/api/auth/signout' className={cn( buttonVariants({ variant: 'default' }) , ' my-4')}>
+        Sign out
+      </Link>
       <ul>
-        <li><span className=' font-bold'>id:</span> {session.user.id}</li>
-        <li><span className=' font-bold'>name:</span> {session.user.name}</li>
-        <li><span className=' font-bold'>mail:</span> {session.user.email}</li>
-        <li><span className=' font-bold'>image:</span> {session.user.image}</li>
-        <li><span className=' font-bold'>username:</span> {session.user.username}</li>
+        <li><span className='font-bold '>id:</span> {session.user.id}</li>
+        <li><span className='font-bold '>name:</span> {session.user.name}</li>
+        <li><span className='font-bold '>mail:</span> {session.user.email}</li>
+        <li><span className='font-bold '>image:</span> {session.user.image}</li>
+        <li><span className='font-bold '>username:</span> {session.user.username}</li>
       </ul>
+
       
     </>
   )
--- src/components/modules/Header/Header.tsx
+++ src/components/modules/Header/Header.tsx
@@ -9,7 +9,7 @@ import Link from "next/link";
 import {Button, buttonVariants} from "@element/Button";
 import { Avatar } from '@/components/elements/Avatar';
 import Image from 'next/image';
-
+import { cn } from "@/lib/utils";
 
 const Header = async () => {
 
@@ -43,11 +43,11 @@ const Header = async () => {
 
 
 
-        <div className="items-center hidden space-x-1 md:flex">
+        <div className="flex items-center space-x-1">
           {session?.user ? (
             <>
-              <Link href='/api/auth/signout' className={buttonVariants({ variant: 'ghost' })}>
-                Sign out
+              <Link href='/api/auth/signout' className={cn( buttonVariants({ variant: 'ghost' }) , ' hidden md:flex ')}>
+                Sign Out
               </Link>
               <Link href='settings'>
                 <Avatar className='w-10 h-10'>
@@ -65,16 +65,6 @@ const Header = async () => {
               </Link>
             </>
           )}
-        </div>
-
-
-        <div className="flex items-center md:hidden">
-          <button className="text-white mobile-menu-button" >
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-          </svg>
-
-          </button>
         </div>
 
       </div>
