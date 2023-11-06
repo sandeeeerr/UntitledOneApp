adfb0c2f79f77bd2673d98c72e66c1eeb44df010
sandeeeerr <sandervries@me.com>
2023-11-06 23:01:08 +0100

    0.2.1 - update Next.js 14

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -7,7 +7,7 @@ import SignIn from "@/components/modules/Signin";
 const page: FC = () => {
   return (
     <div className="absolute inset-0">
-      <div className="h-full max-w-2xl mx-auto flex flex-col items-center justify-center gap-20">
+      <div className="flex flex-col items-center justify-center h-full max-w-2xl gap-20 mx-auto">
         <SignIn />
       </div>
     </div>
