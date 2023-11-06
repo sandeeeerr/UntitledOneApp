4cfb255a9cd68c6ea71fabe9fedba4c9509ae77d
sandeeeerr <sandervries@me.com>
2023-11-06 23:10:17 +0100

    Update package.json

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // import CustomFeed from '@/components/homepage/CustomFeed'
 import { buttonVariants } from "@/components/elements/Button";
 
@@ -10,8 +11,16 @@ export const fetchCache = "force-no-store";
 
 export default async function Home() {
   const session = await getAuthSession();
+=======
+import MusicCard from '@element/MusicCard'
+import CommingSoon from '@module/CommingSoon/CommingSoon'
+import Image from 'next/image'
 
+>>>>>>> parent of 9c4abad (1.2)
+
+export default function Home() {
   return (
+<<<<<<< HEAD
     <>
       <h1 className="">Your feed</h1>
       <Link
@@ -24,4 +33,10 @@ export default async function Home() {
       </Link>
     </>
   );
+=======
+    <div className='text-center'>
+      <CommingSoon />
+    </div>
+  )
+>>>>>>> parent of 9c4abad (1.2)
 }
