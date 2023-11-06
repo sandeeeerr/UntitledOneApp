9c4abadc3bac57814fccae0e78fe1351c563b249
sandeeeerr <sandervries@me.com>
2023-11-06 17:36:31 +0100

    1.2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,26 @@
-import MusicCard from '@element/MusicCard'
-import CommingSoon from '@module/CommingSoon/CommingSoon'
-import Image from 'next/image'
+// import CustomFeed from '@/components/homepage/CustomFeed'
+import { buttonVariants } from '@/components/elements/Button'
 
+import { getAuthSession } from '@/lib/auth'
+import { Home as HomeIcon } from 'lucide-react'
+import Link from 'next/link'
 
-export default function Home() {
+export const dynamic = 'force-dynamic'
+export const fetchCache = 'force-no-store'
+
+export default async function Home() {
+  const session = await getAuthSession()
+
   return (
-    <div className='text-center'>
-      <CommingSoon />
-    </div>
+    <>
+      <h1 className=''>Your feed</h1>
+      <Link
+        className={buttonVariants({
+          className: '',
+        })}
+        href={`/r/create`}>
+        Create Community
+      </Link>
+    </>
   )
 }
