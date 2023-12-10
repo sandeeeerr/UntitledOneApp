f7a88580535f7466b77f6fd840a195f62bc802a3
sandeeeerr <sandervries@me.com>
2023-12-10 11:45:54 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,9 @@ import Link from "next/link";
 import { getAuthSession } from '@/lib/auth'
 import { db } from '@/lib/db'
 import { notFound } from 'next/navigation'
+import Image from "next/image";
+import folderSVG from "@/assets/project.svg";
+
 
 interface PageProps {
   params: {
@@ -40,26 +43,52 @@ const page = async ({ params }: PageProps) => {
 
   return (
     <>
-      <h1 className="">Your feed</h1>
+      <h1 className="mb-12">Your feed</h1>
       
-      <h3>populair projects:</h3>
+      <div className="flow-root mb-4">  
+        <h3 className="float-left">populair communitys:</h3>
+        <div className="float-right">
+        <Link
+          className={buttonVariants({
+            className: "my-2",
+          })}
+          href={`/c/create`}
+        >
+          Create Community
+        </Link>
+        </div>
+      </div>
       
-      
-      <h3>populair communitys:</h3>
-      <Link
-        className={buttonVariants({
-          className: "my-2",
-        })}
-        href={`/c/create`}
-      >
-        Create Community
-      </Link>
-      
-      <ul>
+      <div className="grid grid-cols-2 gap-6 mt-2 mb-12 md:grid-cols-5" id="frameworks-integration">
         {communitys.map(community=>(
-          <a href={`c/${community.name}`}><li>{community.name}</li></a>
+          <a className="grid w-full min-w-[7rem] transform cursor-pointer place-items-center rounded-xl bg-darkGrey px-3 py-2 transition-all hover:scale-105 hover:border-blue-gray-100 hover:bg-blue-gray-50 hover:bg-opacity-25" href={`c/${community.name}`}>
+            <span className="grid w-24 h-24 my-6 place-items-center">
+              {community.name}
+            </span>
+          </a>
         ))}      
-      </ul>
+      </div>
+
+      <div className="flow-root mb-4">  
+        <h3 className="float-left">populair projects:</h3>
+        <div className="float-right">
+        <Link
+          className={buttonVariants({
+            className: "my-2",
+          })}
+          href={`/c/create`}
+        >
+          Create Project
+        </Link>
+        </div>
+      </div>
+      <div className="grid grid-cols-2 gap-6 mt-2 mb-8 md:grid-cols-5" id="frameworks-integration">
+        <Image src={folderSVG} alt="My SVG" height={160} />    
+        <Image src={folderSVG} alt="My SVG" height={160} />   
+        <Image src={folderSVG} alt="My SVG" height={160} />   
+        <Image src={folderSVG} alt="My SVG" height={160} />   
+        <Image src={folderSVG} alt="My SVG" height={160} />   
+      </div>
 
     </>
   )
